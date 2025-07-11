966d0ab7c71f5a40cddcbc9ee30af7ec59607e78
Mateus Lidonis Blanco <mateus.lidonis@gmail.com>
2025-07-11 16:28:06 -0300

    Encontro 7 - Exercício 1

---
+++ Encontro 7/DesafioOperadores/src/desafiooperadores/DesafioOperadores.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package desafiooperadores;
+
+/**
+ *
+ * @author Mateus
+ */
+public class DesafioOperadores {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float x;
+        x = 20+30*5/(50-10*4);
+        System.out.println(x);
+    }
+}
