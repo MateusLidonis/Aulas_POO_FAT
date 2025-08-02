7bc9fd0ef2c21e06d0d1e73ad378364339d79551
Mateus Lidonis Blanco <mateus.lidonis@gmail.com>
2025-08-02 16:09:23 -0300

    Encontro 28 - Exercício 1

---
+++ Encontro 28/CalculadoraInterface/src/calculadorainterface/CalculadoraInterface.java
@@ -0,0 +1,9 @@
+package calculadorainterface;
+
+public class CalculadoraInterface {
+
+    public static void main(String[] args) {
+        calculadorainterfacepanel.CalculoFrame tela = new calculadorainterfacepanel.CalculoFrame();
+        tela.setVisible(true);
+    }
+}
